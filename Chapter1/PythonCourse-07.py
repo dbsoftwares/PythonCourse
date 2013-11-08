313e7e845b3069f4fad81f74e7a9a6886012e1fb
Tóth Péter György <tothpetergyorgy@gmail.com>
2013-11-08 03:56:04 +0100

    Chapter1 commit

---
+++ Chapter1/PythonCourse-07.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# author: Peter Toth
+# web: ptoth.github.io
+# github: github.com/ptoth
+
+## Creating module
+# import myModule
+import myModule
+
+# invoking method from module
+myModule.sayHello()
+print ("\n myModule Version:")
+
+# print module's version
+print(myModule.__version__)
+
+print("\nCourse-07 End")
---
+++ Chapter1/PythonCourse-08.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# author: Peter Toth
+# web: ptoth.github.io
+# github: github.com/ptoth
+
+## Creating module
+# import sys module
+import sys
+import myModule
+
+# print module's version
+print("Version of sys module")
+print(sys.version)
+print("Version of myModule")
+print(myModule.__version__)
+
+print("\nCourse-08 End")
